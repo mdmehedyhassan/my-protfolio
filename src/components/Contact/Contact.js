260bb3115dea33bd79fcbdc06b12f68c53223157
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-05-03 05:22:36 +0600

    just

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,6 +8,18 @@ const Contact = () => {
     return (
         <div className="text-danger pt-5 mt-p">
             <h1 className="text-center border border-info p-2 mt-3 rounded-pill">Contact Me</h1>
+            <div className="row">
+                <div className="col-md-6">
+                    <img className="w-100 pt-3" src={contact} alt="" />
+                </div>
+                <div className="col-md-6 pt-5 mb-5">
+                    <h2 className="text-info">Contact me by email here.</h2>
+                    <input placeholder="Your Name" className="input-group input-lg" type="text" /><br />
+                    <input type="email" placeholder="email" className="input-group input-lg" /><br />
+                    <input style={{ height: '100px' }} placeholder="Description" className="input-group input-lg" type="text" /><br />
+                    <button className="btn-danger p-2">Submit</button>
+                </div>
+            </div>
             <div className="row mt-5 mb-5">
                 <div className="col-md-4 text-center">
                     <div className="m-2 border border-danger">
@@ -43,19 +55,6 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-6">
-                    <img className="w-100 pt-3" src={contact} alt="" />
-                </div>
-                <div className="col-md-6 pt-5 mb-5">
-                    <h2 className="text-info">Contact me by email here.</h2>
-                    <input placeholder="Your Name" className="input-group input-lg" type="text" /><br />
-                    <input type="email" placeholder="email" className="input-group input-lg" /><br />
-                    <input style={{ height: '100px' }} placeholder="Description" className="input-group input-lg" type="text" /><br />
-                    <button className="btn-danger p-2">Submit</button>
-                </div>
-            </div>
-            
         </div>
     );
 };
