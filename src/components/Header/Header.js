ca029980ba71ee6cf33cda3a28027bd32d829427
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-06-26 23:55:31 +0600

    night mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,22 @@
-import { faAddressCard, faDownload, faFileWord, faHome, faMobile, faTasks, faThLarge } from '@fortawesome/free-solid-svg-icons';
+import { faAddressCard, faFileWord, faHome, faMobile, faTasks, faThLarge, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from "react";
 import { Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
 
 const Header = () => {
+    const [theme, setTheme] = useState("light");
+    const [darkMode, setDarkMode] = useState(true);
+
+    const themeToggler = () => {
+        theme === "light" ? setTheme("dark") : setTheme("light");
+    };
     return (
         <div>
             <div className="row align-items-center mb-5">
@@ -14,7 +26,7 @@ const Header = () => {
                 <div className="col-9 justify-content-end ">
                     <Navbar className="justify-content-end  " expand="lg">
                         <Navbar.Toggle className="bg-danger" aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse  id="basic-navbar-nav">
+                        <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="mr-auto" activeKey="/">
                                 <Link className="p-2 text-danger" to="/home"><FontAwesomeIcon icon={faHome} /> Home</Link>
                                 <Link className="p-2 text-danger " to="/about"><FontAwesomeIcon icon={faAddressCard} /> About</Link>
@@ -22,11 +34,17 @@ const Header = () => {
                                 <Link className="p-2 text-danger" to="/resume"><FontAwesomeIcon icon={faFileWord} /> Resume</Link>
                                 <Link className="p-2 text-danger" to="/blog"><FontAwesomeIcon icon={faThLarge} /> Blog</Link>
                                 <Link className="p-2 text-danger" to="/contact"><FontAwesomeIcon icon={faMobile} /> Contact</Link>
-                                <a className="btn btn-danger"    href="https://drive.google.com/file/d/1quuqu9NmjQz4eSFOkHzfX2kLE0TqXeDV/view"><FontAwesomeIcon icon={faDownload} />  Download Resume</a>
                             </Nav>
                         </Navbar.Collapse>
+                        <ThemeProvider theme={theme === "dark" ? lightTheme : darkTheme}>
+                            <GlobalStyles />
+                            <StyledApp>
+                                <span className="p-2" onClick={() => [themeToggler(), setDarkMode(!darkMode)]}>{darkMode ? <span className="btn btn-outline-info">off <FontAwesomeIcon icon={faMoon} /></span> : <span className="btn btn-dark">on <FontAwesomeIcon icon={faMoon} /></span>} </span>
+                            </StyledApp>
+                        </ThemeProvider>
                     </Navbar>
                 </div>
+
             </div>
         </div>
     );
