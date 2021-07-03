e5a928784088ac7ca3a197f79b989470bc2b8595
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-07-03 21:28:43 +0600

    night mode

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,21 +27,22 @@ const Header = () => {
                     <Navbar className="justify-content-end  " expand="lg">
                         <Navbar.Toggle className="bg-danger" aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="mr-auto" activeKey="/">
+                            <Nav className="mr-auto text-right" activeKey="/">
                                 <Link className="p-2 text-danger" to="/home"><FontAwesomeIcon icon={faHome} /> Home</Link>
                                 <Link className="p-2 text-danger " to="/about"><FontAwesomeIcon icon={faAddressCard} /> About</Link>
                                 <Link className="p-2  text-danger" to="/project"><FontAwesomeIcon icon={faTasks} /> Project</Link>
                                 <Link className="p-2 text-danger" to="/resume"><FontAwesomeIcon icon={faFileWord} /> Resume</Link>
                                 <Link className="p-2 text-danger" to="/blog"><FontAwesomeIcon icon={faThLarge} /> Blog</Link>
                                 <Link className="p-2 text-danger" to="/contact"><FontAwesomeIcon icon={faMobile} /> Contact</Link>
+                                <ThemeProvider theme={theme === "dark" ? lightTheme : darkTheme}>
+                                    <GlobalStyles />
+                                    <StyledApp>
+                                        <span className="p-2" onClick={() => [themeToggler(), setDarkMode(!darkMode)]}>{darkMode ? <span className="btn btn-outline-info">off <FontAwesomeIcon icon={faMoon} /></span> : <span className="btn btn-dark">on <FontAwesomeIcon icon={faMoon} /></span>} </span>
+                                    </StyledApp>
+                                </ThemeProvider>
                             </Nav>
                         </Navbar.Collapse>
-                        <ThemeProvider theme={theme === "dark" ? lightTheme : darkTheme}>
-                            <GlobalStyles />
-                            <StyledApp>
-                                <span className="p-2" onClick={() => [themeToggler(), setDarkMode(!darkMode)]}>{darkMode ? <span className="btn btn-outline-info">off <FontAwesomeIcon icon={faMoon} /></span> : <span className="btn btn-dark">on <FontAwesomeIcon icon={faMoon} /></span>} </span>
-                            </StyledApp>
-                        </ThemeProvider>
+
                     </Navbar>
                 </div>
 
